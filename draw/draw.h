dc2c4d81e7913765e390386665442fbf6a2e1403
Isaac <isaac.yeo.linux@gmail.com>
2020-03-06 20:00:16 +0800

    Added make_button declaration in header file

--- draw/draw.h
+++ draw/draw.h
@@ -1,5 +1,8 @@
 #ifndef INC_BASE
-#include "base.h"
+#include "../base.h"
+#include "../basic.h"
 #endif
 
 void buf_putstr(char *str);
+void make_button(struct spos butpos, char **sarr /* Array of C strings */,
+	size_t arrlen, short int w, short int h);
