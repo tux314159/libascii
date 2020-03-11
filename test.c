1460ee9bb280d4cdc96c752d6cb6faf8540eea61
Isaac <isaac.yeo.linux@gmail.com>
2020-03-11 20:56:12 +0800

    Test demonstrates use of buttons, but not very clearly

--- test.c
+++ test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "api.h"
 
+void say_hi(void)
+{
+	buf_putstr("HIIII");
+	return;
+}
+
 int main(void)
 {
 	startraw();
@@ -28,8 +34,10 @@ int main(void)
 	/* Page 2 */
 	char *buttext[6] = {"Hello!", "Bye..."};
 	button_create((struct spos){4, 5}, buttext, 2, 10, 3);
+	button_bind(1, &say_hi);
 	curs_mov(getwinrows() / 2, getwincols() / 2 - 5);
 	buf_putstr("libascii v0");
+	button_call(1);
 	curs_mov(getwinrows(), 0);
 	buf_putstr("Type something and press enter: ");
 	buf_write();
