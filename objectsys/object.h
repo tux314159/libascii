1b8396832d56abd3fea7b951e234abeaa03a6456
Isaac <isaac.yeo.linux@gmail.com>
2020-03-12 18:54:47 +0800

    Fixed double-include error

--- objectsys/object.h
+++ objectsys/object.h
@@ -1,4 +1,6 @@
+#ifndef INC_BASE
 #include "../base.h"
+#endif
 short int object_create(char rep, struct spos pos);
 void object_del(short int id);
 void object_mov(short int id, struct spos newpos);
