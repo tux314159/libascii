0e108e97253e2dac20a55f720b8855f89da5f6bb
Isaac <isaac.yeo.linux@gmail.com>
2020-03-12 15:18:18 +0800

    Added button labels

--- draw/button.c
+++ draw/button.c
@@ -2,15 +2,18 @@
 short int button_create(struct spos butpos, char **sarr /* Array of C strings */,
 	size_t arrlen, short int w, short int h)
 {
+	w += 2;
+	h += 1;
+
 	int r = _gls->cpos.r;
 	int c = _gls->cpos.c;
 	short int id;
 	struct button cbut;
+	char tmp[4];
 
-	curs_mov(butpos.r, butpos.c);
-	for (int i = 0; i < w; i++) buf_putstr("-");
-	curs_mov(butpos.r + h + 1, butpos.c);
-	for (int i = 0; i < w; i++) buf_putstr("-");
+	id = ++_gls->idmax;
+	if (id > MAX_BUTTONS) return -1; /* Error! */
+
 	for (int i = 1; i <= h; i++) {
 		curs_mov(butpos.r + i, butpos.c);
 		buf_putstr("|");
@@ -18,13 +21,23 @@ short int button_create(struct spos butpos, char **sarr /* Array of C strings */
 		buf_putstr("|");
 	}
 
+	curs_mov(butpos.r, butpos.c);
+	for (int i = 0; i < w; i++)
+		buf_putstr("-");
+	curs_mov(butpos.r + h, butpos.c);
+	for (int i = 0; i < w; i++)
+		buf_putstr("-");
+
 	for (int i = 0; i < arrlen && i < h; i++) {
 		curs_mov(butpos.r + i + 1, butpos.c + 1);
 		buf_putstr(sarr[i]);
 	}
 
-	id = ++_gls->idmax;
-	if (id > MAX_BUTTONS) return -1; /* Error! */
+	/* Put button id there */
+	curs_mov(butpos.r, butpos.c);
+	sprintf(tmp, "%d", id);
+	buf_putstr(tmp);
+
 	cbut.id = id;
 	cbut.action = NULL;
 	_gls->buttons[id - 1] = cbut;
