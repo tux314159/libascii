a9b712e215d01df1ab15ac6cc0c57836c08d4eae
Isaac <isaac.yeo.linux@gmail.com>
2020-03-12 18:38:43 +0800

    Added function to move objects

--- objectsys/object.c
+++ objectsys/object.c
@@ -10,6 +10,13 @@ void object_del(short int id)
 {
 	vector_erase(_gls->objects, id - 1);
 	for (int i = id; i < _gls->objects->len - 1; i++)
-		vector_getptr(_gls->objects, i, struct button)->id -= 1;
+		vector_getptr(_gls->objects, i, struct object)->id -= 1;
+	_gls->obj_idmax -= 1;
+	return;
+}
+
+void object_mov(short int id, struct spos newpos)
+{
+	vector_getptr(_gls->objects, id - 1, struct object)->pos = newpos;
 	return;
 }
--- objectsys/object.h
+++ objectsys/object.h
@@ -1,1 +1,4 @@
 #include "../base.h"
+short int object_create(char rep, struct spos pos);
+void object_del(short int id);
+void object_mov(short int id, struct spos newpos);
