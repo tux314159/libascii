881a3d6c28ef33f107d8ea774b11c2a01aab07b8
Isaac <isaac.yeo.linux@gmail.com>
2020-04-06 22:09:28 +0800

    Moved a basic function

--- draw/buf_putstr.c
+++
@@ -1,7 +0,0 @@
-
-#include "../base.h"
-void buf_putstr(char *str)
-{
-	str_append(_gls->abuf, str, strlen(str));
-	return;
-}
--- draw/draw.h
+++ draw/draw.h
@@ -3,7 +3,6 @@
 #include "../basic.h"
 #endif
 
-void buf_putstr(char *str);
 short int button_create(struct spos butpos, char **sarr /* Array of C strings */,
 	size_t arrlen, short int w, short int h);
 void button_bind(short int id, void_fnptr action); /* The function pointer action should be global! */
