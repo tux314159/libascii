240cf668f8ac40d8cd9123177ac0a2c0357f06a9
Isaac <isaac.yeo.linux@gmail.com>
2020-06-20 10:26:44 +0800

    Added the new grid_check function to the header file

--- objectsys/object.h
+++ objectsys/object.h
@@ -1,10 +1,12 @@
 #ifndef INC_BASE
 #include "../base.h"
 #endif
+
+/* Directions */
+enum DIR {N, NE, E, SE, S, SW, W, NW, ALL};
+
 short int object_create(char rep, struct spos pos);
 void object_del(short int id);
 void object_mov(short int id, struct spos newpos);
 struct object object_getattr(short int id);
-
-/* Directions */
-enum DIR {N, NE, E, SE, S, SW, W, NW, ALL};
+struct gridtest object_celltest(short int id, enum DIR dir);
