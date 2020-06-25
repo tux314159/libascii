24da6eb7436c73503f58d89112ec2466000eb55a
Isaac <isaac.yeo.linux@gmail.com>
2020-06-25 13:42:24 +0800

    Whoops

--- grid/grid.h
+++ grid/grid.h
@@ -14,5 +14,5 @@ bool txt_grid_deinit(void);
 /* Operations */
 struct object *obj_grid_cell_add(struct object obj, struct spos pos);
 struct obj_gridtest obj_grid_cell_check(short int id, enum DIR dir);
-void *txt_grid_cell_add(struct object obj, struct spos pos);
+void txt_grid_cell_add(char in, struct spos pos);
 struct txt_gridtest txt_grid_cell_check(short int id, enum DIR dir);
