780607dd8a422649b34142635d4a19783ee5f24c
Isaac <isaac.yeo.linux@gmail.com>
2020-06-26 09:33:20 +0800

    NOT WORKING!

--- basic.c
+++ basic.c
@@ -103,40 +103,50 @@ char scankey(void)
 	return t[0];
 }
 
+static void read_arrows(char *str)
+{
+	char t[2] = "\0";
+	/* Assume the \x1b has been read! */
+	read(STDIN, &t, 1);
+	if (t[0] != '[') /* Not an escape code */
+		return;
+	read(STDIN, &t, 1);
+	switch (t[0]) {
+	case 'A':
+		return;
+		break;
+	case 'B':
+		return;
+		break;
+	case 'C':
+
+}
+
 void scanstr(string **store, char delim)
 {
 	int scanlen = 0;
 	char t[4] = "\0\0\0";
-	for (;;) {
+	while (t[0] != delim) {
 		read(STDIN, &t, 1);
 		scanlen += 1;
-		if (t[0] == delim)
-			break;
-		else {
-			if (t[0] == 127 && scanlen != 0) {
-				scanlen -= 1; /* The backspace character itself */
-				if (scanlen != 0) {
-					scanlen -= 1; /* Again; this is minusing off the previous character */
-					t[0] = CTRL_KEY('H');
-					t[1] = ' ';
-					t[2] = CTRL_KEY('H');
-				}
-			}
-
-			/* Ignore arrow keys */
-			if (t[0] == '\x1b') {
-				read(STDIN, &t, 2);
-				if (t[1] == 'A' || t[1] == 'B' || t[1] == 'C' || t[1] == 'D') {
-					scanlen -= 1;
-					t[0] = '\0';
-					t[1] = '\0';
-					t[2] = '\0';
-				}
-			}
-			if (_lascii->echo)
-				write(STDOUT, t, 3);
-			str_append(*store, t, 3);
+		if (t[0] == 127 && --scanlen > 0) {
+			scanlen -= 1; /* Again; this is minusing off the previous character */
+			t[0] = CTRL_KEY('H');
+			t[1] = ' ';
+			t[2] = CTRL_KEY('H');
 		}
+
+		/* Ignore special keys */
+		if (t[0] == '\x1b') {
+			read(STDIN, &t, 1);
+			scanlen -= 1;
+			t[0] = '\0';
+			t[1] = '\0';
+			t[2] = '\0';
+		}
+		if (_lascii->echo)
+			write(STDOUT, t, 3);
+		str_append(*store, t, 3);
 	}
 	curs_mov(_lascii->cpos);
 	return;
