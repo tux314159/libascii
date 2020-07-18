237dc515ba7f62f4052e587075e9546ab6de2fb3
Isaac <isaac.yeo.linux@gmail.com>
2020-07-18 22:27:27 +0800

    CPP indentation

--- basic.h
+++ basic.h
@@ -1,6 +1,9 @@
 #ifndef INC_BASE
 #    include "base.h"
 #endif
+#ifndef INC_GRID
+#    include "grid/grid.h"
+#endif
 #ifndef INC_IO
 #    include "io/io.h"
 #endif
@@ -11,3 +14,5 @@ void paintscreen(void);
 #define setecho() (__lascii->echo = 1)
 #define setnoecho() (__lascii->echo = 0)
 #define char2str(x) ((char[2]){(x), '\0'})
+
+#define INC_BASIC
