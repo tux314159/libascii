56726e4d7b27e7818de8bb8eca701d8330b57836
Isaac <isaac.yeo.linux@gmail.com>
2020-07-20 08:25:07 +0800

    Expose grid init functions

--- grid/grid.h
+++ grid/grid.h
@@ -6,6 +6,12 @@
 /* Directions */
 enum DIR {N, NE, E, SE, S, SW, W, NW};
 
+/* Init */
+struct vector ***obj_grid_init(void);
+void obj_grid_deinit(void);
+char **txt_grid_init(void);
+void txt_grid_deinit(void);
+
 /* Operations */
 struct object *obj_grid_cell_add(struct object obj, struct spos pos);
 struct obj_gridtest obj_grid_cell_check(short int id, enum DIR dir);
